e5530dd9f37ca8f89ed4408940a8571f5716997b
djbreen7 <daniel.breen@royaljay.net>
2019-02-27 20:40:34 -0700

    Update Gulpfile.js for Gulp 4

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var gulp   = require('gulp');
+var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var mocha  = require('gulp-mocha');
+var mocha = require('gulp-mocha');
 
-gulp.task('lint', function () {
+function lint () {
     return gulp
         .src([
             'generators/app/index.js',
-            'test.js',
+            'test/test.js',
             'Gulpfile.js'
         ])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-});
+}
 
-gulp.task('test', ['lint'], function () {
+gulp.task('test', gulp.series([lint]), function () {
     return gulp
         .src('test/test.js')
         .pipe(mocha({
