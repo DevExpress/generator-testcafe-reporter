7c0e3444bcb1b17f50d9fb7a31be254de000ddf6
djbreen7 <daniel.breen@royaljay.net>
2019-03-02 12:23:49 -0700

    Fix Indentation

--- generators/app/templates/Gulpfile.js
+++ generators/app/templates/Gulpfile.js
@@ -18,23 +18,23 @@ function lint () {
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-};
+}
 
 function build () {
     return gulp
-    .src('src/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('lib'));
+        .src('src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('lib'));
 }
 
 function test () {
     return gulp
-    .src('test/test.js')
-    .pipe(mocha({
-        ui:       'bdd',
-        reporter: 'spec',
-        timeout:  typeof v8debug === 'undefined' ? 20000 : Infinity // NOTE: disable timeouts in debug
-    }));
+        .src('test/test.js')
+        .pipe(mocha({
+            ui:       'bdd',
+            reporter: 'spec',
+            timeout:  typeof v8debug === 'undefined' ? 20000 : Infinity // NOTE: disable timeouts in debug
+        }));
 }
 
 function preview () {
