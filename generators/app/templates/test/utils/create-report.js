d411814e3af5bd73442cec258313541f039e7f2c
Pavel <wabloxa@yandex.ru>
2020-04-16 17:21:29 +0300

    Disable errors coloring

--- generators/app/templates/test/utils/create-report.js
+++ generators/app/templates/test/utils/create-report.js
@@ -15,6 +15,23 @@ module.exports = async function createReport (withColors) {
 
     plugin.chalk.enabled = !plugin.noColors && withColors;
 
+    // NOTE: disable errors coloring
+    // because errors rendering is done by TestCafe
+    // and can be changed regardless of the plugin state
+    if (plugin.chalk.enabled) {
+        const origFormatError = plugin.formatError;
+
+        plugin.formatError = function () {
+            plugin.chalk.enabled = false;
+
+            const result = origFormatError.apply(plugin, arguments);
+
+            plugin.chalk.enabled = true;
+
+            return result;
+        };
+    }
+
     for (const call of reporterTestCalls)
         await plugin[call.method].apply(plugin, call.args);
 
