62862660437047e7d2970adfa7ddd995593714de
Pavel <wabloxa@yandex.ru>
2020-04-17 17:32:03 +0300

    More information in reporter methods

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -8,12 +8,17 @@ export default function () {
             this.write(util.inspect({ startTime, userAgents, testCount })).newline();
         },
 
-        async reportFixtureStart (name) {
-            this.write(util.inspect({ name })).newline();
+        async reportFixtureStart (name, path, meta) {
+            this.write(util.inspect({ name, path, meta })).newline();
         },
 
-        async reportTestDone (name, testRunInfo) {
-            this.write(util.inspect({ name, testRunInfo })).newline();
+        async reportTestStart (name, meta) {
+            // NOTE: This method is optional.
+            this.write(util.inspect({ name, meta })).newline();
+        },
+
+        async reportTestDone (name, testRunInfo, meta) {
+            this.write(util.inspect({ name, testRunInfo, meta })).newline();
         },
 
         async reportTaskDone (endTime, passed, warnings, result) {
